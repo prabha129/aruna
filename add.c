5fdb5468ccec960b7625d760f4de5f40dbb1c045
prabha129 <35652743+prabha129@users.noreply.github.com>
2018-03-01 15:52:38 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,18 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+scanf("%d%d",&a,&b);
+c=a*b;
+if(c%2==0)
+{
+printf("even");
+}
+else
+{
+printf("odd");
+}
+getch();
+}
